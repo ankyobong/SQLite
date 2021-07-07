06558cd928e440f14070c9a047c6b433524dc3f2
ankyobong <ankyobong@gmail.com>
2021-07-07 19:08:02 +0900

    대량삽입 성공은 했는데.... 아 뭔가 맘에안듬

--- SQLite/sqlite_study.py
+++ SQLite/sqlite_study.py
@@ -1,2 +1,19 @@
-.mode csv
-.import /Users/quackit/sqlite/dumps/catalog.csv Catalog  #\ 아니라 그냥 / 임 조심... CSV파일 불러와서 테이블 만드는 거  스키마 생성은 어렵
+import sqlite3
+import csv
+# db 파일 여는법
+a = sqlite3.connect('testdb.db')
+cur = a.cursor()    # 해당스키마를 바라보게함
+a.execute("INSERT into Artists values (?, ?)", (None, 'kyobong'))    # 단일 삽입
+
+with open('testdb.csv', 'r', encoding='utf-8') as ifp:
+    csv_data = csv.reader(ifp)
+    # t_csv_data = [tuple(row) for row in csv_data]
+    t_csv_data = list(csv_data)
+    # print(t_csv_data)
+    for i in t_csv_data:
+        # print(i[0])
+        a.execute("INSERT into Artists values ( ?, ?)", (None, i[0]))    # 대량삽입
+
+
+for row in cur.execute('SELECT * FROM Artists'):  # sql
+    print(row)
