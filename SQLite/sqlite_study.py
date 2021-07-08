5457e33aafafcad9e44c5009ba107e29391f1eed
ankyobong <ankyobong@gmail.com>
2021-07-08 09:19:49 +0900

    대량삽입

--- SQLite/sqlite_study.py
+++ SQLite/sqlite_study.py
@@ -6,13 +6,9 @@ cur = a.cursor()    # 해당스키마를 바라보게함
 a.execute("INSERT into Artists values (?, ?)", (None, 'kyobong'))    # 단일 삽입
 
 with open('testdb.csv', 'r', encoding='utf-8') as ifp:
-    csv_data = csv.reader(ifp)
-    # t_csv_data = [tuple(row) for row in csv_data]
-    t_csv_data = list(csv_data)
-    # print(t_csv_data)
-    for i in t_csv_data:
-        # print(i[0])
-        a.execute("INSERT into Artists values ( ?, ?)", (None, i[0]))    # 대량삽입
+    csv_data = list(csv.reader(ifp))
+    for i in csv_data:
+        a.execute("INSERT into Artists values ( ?, ?)", (None, ','.join(i)))    # 대량삽입
 
 
 for row in cur.execute('SELECT * FROM Artists'):  # sql
