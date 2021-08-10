e8e96cbb838db5d579ebace3728a33942121085f
ankyobong <ankyobong@gmail.com>
2021-08-10 16:35:42 +0900

    seaborn test

--- seaborn/seaborn_study.py
+++ seaborn/seaborn_study.py
@@ -1,17 +1,20 @@
+
 import seaborn as sns
 import matplotlib.pyplot as plt
 import pandas
 import os
+import matplotlib.font_manager as fm
+
 
 # plt.rc('font', family='NanumBarunGothic')
 # plt.rcParams["figure.figsize"] = (20, 20)
 
-sns.set_theme(style=None)
-
+# sns.set_theme(style=None)
+# sns.set_palette("Blues")
 # Load the example dataset for Anscombe's quartet
 # df = sns.load_dataset("anscombe")
 # df = pandas.read_csv("anscombe.csv")
-df = pandas.read_excel("C:\\Users\\argos\\Desktop\\bongsplugin\\plug-in-test\\seaborn\\anscombe.xlsx",sheet_name="Sheet1",engine='openpyxl')
+df = pandas.read_excel("C:\\Users\\argos\\Desktop\\bongsplugin\\plug-in-test\\seaborn\\anscombe.xlsx", engine='openpyxl')
 
 
 # print(df)
@@ -19,17 +22,21 @@ df = pandas.read_excel("C:\\Users\\argos\\Desktop\\bongsplugin\\plug-in-test\\se
 
 # t = 'x="x", y="y", data=df, hue="dataset"
 # Show the results of a linear regression within each dataset
-# sns.lmplot(x="x", y="y", col="dataset", hue="dataset", data=df,
-#            col_wrap=2, ci=None, height=4,)
-# sns.scatterplot(x="x", y="y", data=df, hue="dataset")
+sns.set_theme(style='dark')
+# x = "'Blues', 8, .75"
+# sns.set_palette('Blues', None, None)
+# s = sns.lmplot(x="x", y="y", col="dataset", hue="dataset", data=df,
+#                col_wrap=2, ci=None, height=4, )
+# s = sns.scatterplot(x="x", y="y", data=df, hue="dataset")
+# s.set_title('한국어')
 # sns.lineplot(x="x", y="y", data=df, hue="dataset")
 
 # sns.displot(x="x", y="y", data=df, hue="dataset")
 # sns.histplot(x="x", y="y", data=df, hue="dataset")
 # sns.kdeplot(x="x", y="y", data=df, hue="dataset")
 sns.ecdfplot(x="x", y="y", data=df, hue="dataset")
-# sns.rugplot(x="x", y="y", data=df, hue="dataset")
-# sns.distplot(x="x", y="y", data=df, hue="dataset")
+# s = sns.rugplot(x="x", y="y", data=df)     # 이상하게나오는 부분
+# sns.distplot(x="x", y="y", data=df, hue="dataset")      # 안나옴
 
 # sns.catplot(x="x", y="y", data=df, hue="dataset")
 # sns.stripplot(x="x", y="y", data=df, hue="dataset")
@@ -39,14 +46,26 @@ sns.ecdfplot(x="x", y="y", data=df, hue="dataset")
 # sns.boxenplot(x="x", y="y", data=df, hue="dataset")
 # sns.pointplot(x="x", y="y", data=df, hue="dataset")
 # sns.barplot(x="x", y="y", data=df, hue="dataset")
-# sns.countplot(x="x", y="y", data=df, hue="dataset")
-#
-# sns.lmplot(x="x", y="y", data=df, hue="dataset")
-# sns.regplot(x="x", y="y", data=df, hue="dataset")
-# sns.residplot(x="x", y="y", data=df, hue="dataset")
+# sns.countplot(x="x", y="y", data=df, )     # 에러
 
+# sns.lmplot(x="x", y="y", data=df, hue="dataset")
+# sns.regplot(x="x", y="y", data=df)
+# sns.residplot(x="x", y="y", data=df)
+# sns.set_context("paper")
+# s.set_xticklabels(None)
+# s.set_theme(style='bark')
+# s.set_xlabels(None, fontsize=None)
+# plt.xlabel("20", fontsize=None)
+plt.savefig('test6.png')
+# plt.rc('font', family='Malgun Gothic')
+# plt.rc('axes', unicode_minus=False)
+# plt.title("this is 한국말")
+# x = "엑스축"
+# plt.ylabel("와이축")
+# plt.xlabel("엑스축")
+# plt.show()
 # df.to_excel("anscombe.xlsx")
 # df.to_csv("anscombe.csv")
-plt.savefig('test5.png')
+# plt.savefig('test6.png', encoding="utf-8")
 
 
